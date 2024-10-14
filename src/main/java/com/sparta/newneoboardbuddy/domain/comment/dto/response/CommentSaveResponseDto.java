87e4b1ea27e47bbcfbb43953ffee95e6c282f0c1
oht <duduio2050@gmail.com>
2024-10-14 20:03:15 +0900

    [feat] comment 기본 파일 생성

---
+++ src/main/java/com/sparta/newneoboardbuddy/domain/comment/dto/response/CommentSaveResponseDto.java
@@ -0,0 +1,11 @@
+package com.sparta.newneoboardbuddy.domain.comment.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentSaveResponseDto {
+}
