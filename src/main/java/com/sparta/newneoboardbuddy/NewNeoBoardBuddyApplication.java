e86e1b0ac052b44b8650b1a628c3aa12a7129ffa
Sun choi <csun2772@naver.com>
2024-10-14 16:50:32 +0900

    first commit

---
+++ src/main/java/com/sparta/newneoboardbuddy/NewNeoBoardBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.newneoboardbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewNeoBoardBuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewNeoBoardBuddyApplication.class, args);
+    }
+
+}
