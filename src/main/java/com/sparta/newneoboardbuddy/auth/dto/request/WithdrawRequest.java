c15dc28e0c110c5a4ccd0a770069d4713f2c248a
kkw2238 <kkw2238@naver.com>
2024-10-14 21:14:36 +0900

    [feat] Auth Controller / Service 추가

---
+++ src/main/java/com/sparta/newneoboardbuddy/auth/dto/request/WithdrawRequest.java
@@ -0,0 +1,12 @@
+package com.sparta.newneoboardbuddy.auth.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class WithdrawRequest {
+    String password;
+}
