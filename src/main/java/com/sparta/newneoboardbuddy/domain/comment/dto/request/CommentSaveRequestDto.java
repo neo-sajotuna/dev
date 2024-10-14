81a848fcfbcaf030915afc15662ad6335cf4c89f
oht <duduio2050@gmail.com>
2024-10-15 00:35:34 +0900

    [feat] 댓글 저장 request, response dto 생성

--- src/main/java/com/sparta/newneoboardbuddy/domain/comment/dto/request/CommentSaveRequestDto.java
+++ src/main/java/com/sparta/newneoboardbuddy/domain/comment/dto/request/CommentSaveRequestDto.java
@@ -1,5 +1,7 @@
 package com.sparta.newneoboardbuddy.domain.comment.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,4 +10,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CommentSaveRequestDto {
+
+    @NotNull(message = "카드 id 값은 필수 파라미터 입니다.")
+    private Long cardId;
+
+    @NotBlank(message = "comment 값은 필수 파라미터 입니다.")
+    private String comment;
+
 }
