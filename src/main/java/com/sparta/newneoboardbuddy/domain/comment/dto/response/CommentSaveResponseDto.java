6ebc881a4b7db259a4c08142492c468b29c96ab3
oht <duduio2050@gmail.com>
2024-10-15 00:35:34 +0900

    [feat] 댓글 저장 request, response dto 생성

--- src/main/java/com/sparta/newneoboardbuddy/domain/comment/dto/response/CommentSaveResponseDto.java
+++ src/main/java/com/sparta/newneoboardbuddy/domain/comment/dto/response/CommentSaveResponseDto.java
@@ -2,10 +2,15 @@ package com.sparta.newneoboardbuddy.domain.comment.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Getter
 @AllArgsConstructor
-@NoArgsConstructor
 public class CommentSaveResponseDto {
+
+    private final Long commentId;
+
+    private final LocalDateTime createdAt;
+
 }
