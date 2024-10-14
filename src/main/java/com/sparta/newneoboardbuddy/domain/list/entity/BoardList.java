d5c510611ea068b216be7946204d740aeeb0e862
kkw2238 <kkw2238@naver.com>
2024-10-14 20:02:33 +0900

    [feat] BoardList Entity 작성

---
+++ src/main/java/com/sparta/newneoboardbuddy/domain/list/entity/BoardList.java
@@ -0,0 +1,24 @@
+package com.sparta.newneoboardbuddy.domain.list.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "list")
+public class BoardList {
+    @Id
+    private Long id;
+
+    private String title;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id", nullable = false)
+    private Board board;
+
+    public BoardList(String title) {
+        this.title = title;
+    }
+}
