1b59b8bfe826a4e5dbb6d0c41185c830358318a2
oht <duduio2050@gmail.com>
2024-10-15 13:54:54 +0900

    [fix] 커밋 실수 수정

--- src/main/java/com/sparta/newneoboardbuddy/domain/member/rpository/MemberRepository.java
+++ src/main/java/com/sparta/newneoboardbuddy/domain/member/rpository/MemberRepository.java
@@ -1,11 +1,12 @@
 package com.sparta.newneoboardbuddy.domain.member.rpository;
 
+import com.sparta.newneoboardbuddy.common.dto.AuthUser;
 import com.sparta.newneoboardbuddy.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByUserIdAndSpaceId(Long userId, Long spaceId);
+    Optional<Member> findByUserIdAndWorkspaceId(Long userId, Long workspaceId);
 
 }
