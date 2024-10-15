33cb388a9178034b9fb35ee40c37dcc2ad2ab238
kkw2238 <kkw2238@naver.com>
2024-10-15 17:18:28 +0900

    [feat] 폴더 수정 & List 기능 추가중

---
+++ src/main/java/com/sparta/newneoboardbuddy/domain/list/service/BoardListService.java
@@ -0,0 +1,15 @@
+package com.sparta.newneoboardbuddy.domain.list.service;
+
+import com.sparta.newneoboardbuddy.common.dto.AuthUser;
+import com.sparta.newneoboardbuddy.domain.list.dto.request.BoardListRequest;
+import com.sparta.newneoboardbuddy.domain.list.dto.response.BoardListResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardListService {
+
+    public BoardListResponse createList(AuthUser authUser, BoardListRequest boardListRequest) {
+
+
+    }
+}
