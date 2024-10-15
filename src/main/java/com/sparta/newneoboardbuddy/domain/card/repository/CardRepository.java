3193a0edcb045a4271c9fcbcaea6b532a9656df3
HyunKook <gusrnr5153@gmail.com>
2024-10-15 18:29:32 +0900

    개선중 2

--- src/main/java/com/sparta/newneoboardbuddy/domain/card/repository/CardRepository.java
+++ src/main/java/com/sparta/newneoboardbuddy/domain/card/repository/CardRepository.java
@@ -4,6 +4,6 @@ import com.sparta.newneoboardbuddy.domain.card.entity.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface CardRepository extends JpaRepository<Card, Long> {
+public interface CardRepository extends JpaRepository<Card, Long>, CardRepositoryCustom {
 
 }
