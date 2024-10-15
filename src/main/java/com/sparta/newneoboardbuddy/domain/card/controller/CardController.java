de07781e847cf9ca956368cea4ec6c6f65bac6b1
HyunKook <gusrnr5153@gmail.com>
2024-10-15 16:22:39 +0900

    [FEAT/FIX] getCardDetail 구현 / memberService 수정

--- src/main/java/com/sparta/newneoboardbuddy/domain/card/controller/CardController.java
+++ src/main/java/com/sparta/newneoboardbuddy/domain/card/controller/CardController.java
@@ -4,9 +4,11 @@ import com.sparta.newneoboardbuddy.common.dto.AuthUser;
 import com.sparta.newneoboardbuddy.domain.card.dto.request.CardCreateRequest;
 import com.sparta.newneoboardbuddy.domain.card.dto.request.CardUpdateRequest;
 import com.sparta.newneoboardbuddy.domain.card.dto.response.CardCreateResponse;
+import com.sparta.newneoboardbuddy.domain.card.dto.response.CardDetailResponse;
 import com.sparta.newneoboardbuddy.domain.card.dto.response.CardUpdateResponse;
 import com.sparta.newneoboardbuddy.domain.card.service.CardService;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -32,6 +34,11 @@ public class CardController {
             @AuthenticationPrincipal AuthUser authUser,
             @RequestBody CardUpdateRequest request){
         return ResponseEntity.ok(cardService.updateCard(cardId, authUser, request));
+    }
+
+    @GetMapping("/cards/{cardId}")
+    public ResponseEntity<CardDetailResponse> getCard(@PathVariable Long cardId){
+        return ResponseEntity.ok(cardService.getCardDetails(cardId));
     }
 
 
