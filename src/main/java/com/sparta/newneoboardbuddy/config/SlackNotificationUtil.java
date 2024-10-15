4bc827fa27e1ccd709729b358b4a2bd036f45e68
kkw2238 <kkw2238@naver.com>
2024-10-15 13:57:44 +0900

    [feat] Slack연동 구현
    1. 신규 회원 가입시 Slack에 알림을 전송하는 기능 추가
    
    2. Properties에 URL을 추가해야 정상적으로 동작합니다.

---
+++ src/main/java/com/sparta/newneoboardbuddy/config/SlackNotificationUtil.java
@@ -0,0 +1,50 @@
+package com.sparta.newneoboardbuddy.config;
+
+import com.slack.api.Slack;
+import com.slack.api.webhook.WebhookResponse;
+import com.sparta.newneoboardbuddy.domain.user.entity.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Slf4j(topic = "notice")
+public class SlackNotificationUtil {
+    @Value("${slack.webhook.url}")
+    private String web_url;
+
+    private final String PAY_LOAD_HEAD = "{ \"text\" : \" ";
+    private final String PAY_LOAD_TAIL = "\"}";
+
+    private final Slack slack;
+
+    public SlackNotificationUtil() {
+        slack = Slack.getInstance();
+    }
+
+    public void sendNewUser(User user) {
+        String payload = String.format(PAY_LOAD_HEAD +
+                "신규 가입 발생 : " +
+                "id : %d," +
+                "email : %s," +
+                "userRole : %s" +
+                PAY_LOAD_TAIL, user.getId(), user.getEmail(), user.getUserRole().name());
+
+        sendNotice(payload);
+    }
+
+    /**
+     * Slack에 Payload에 담긴 메시지를 전송하는 메서드
+     * @param payload Slack에 전송할 알림/메시지
+     */
+    private void sendNotice(String payload) {
+        try {
+            WebhookResponse response = slack.send(web_url, payload);
+        }
+        catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+}
