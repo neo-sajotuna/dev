4e53ad653a448c0b91424b854cef46486fa342c6
HyunKook <gusrnr5153@gmail.com>
2024-10-15 22:54:57 +0900

    [feat] 카드 생성 테스트 코드 작성 완료

--- src/main/java/com/sparta/newneoboardbuddy/domain/list/service/BoardListService.java
+++ src/main/java/com/sparta/newneoboardbuddy/domain/list/service/BoardListService.java
@@ -3,12 +3,14 @@ package com.sparta.newneoboardbuddy.domain.list.service;
 import com.sparta.newneoboardbuddy.common.dto.AuthUser;
 import com.sparta.newneoboardbuddy.domain.list.dto.request.BoardListRequest;
 import com.sparta.newneoboardbuddy.domain.list.dto.response.BoardListResponse;
+import com.sparta.newneoboardbuddy.domain.list.entity.BoardList;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BoardListService {
 
     public BoardListResponse createList(AuthUser authUser, BoardListRequest boardListRequest) {
+        return new BoardListResponse();
 
 
     }
