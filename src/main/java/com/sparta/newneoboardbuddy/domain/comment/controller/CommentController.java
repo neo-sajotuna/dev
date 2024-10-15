e1233143f65cceaaeb14fa243620a23097fe5940
oht <duduio2050@gmail.com>
2024-10-15 13:53:06 +0900

    [feat] 댓글 생성, 수정, 삭제 기능 구현

--- src/main/java/com/sparta/newneoboardbuddy/domain/comment/controller/CommentController.java
+++ src/main/java/com/sparta/newneoboardbuddy/domain/comment/controller/CommentController.java
@@ -1,9 +1,15 @@
 package com.sparta.newneoboardbuddy.domain.comment.controller;
 
+import com.sparta.newneoboardbuddy.common.dto.AuthUser;
+import com.sparta.newneoboardbuddy.common.dto.response.CommonResponseDto;
+import com.sparta.newneoboardbuddy.domain.comment.dto.request.CommentUpdateRequestDto;
+import com.sparta.newneoboardbuddy.domain.comment.dto.request.CommentSaveRequestDto;
+import com.sparta.newneoboardbuddy.domain.comment.dto.response.CommentSaveResponseDto;
 import com.sparta.newneoboardbuddy.domain.comment.service.CommentService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/comment")
@@ -12,4 +18,23 @@ public class CommentController {
 
     private final CommentService commentService;
 
+    @PostMapping
+    public CommonResponseDto<CommentSaveResponseDto> saveComment(@AuthenticationPrincipal AuthUser authUser, @Valid @RequestBody CommentSaveRequestDto commentSaveRequestDto){
+        return CommonResponseDto.success(
+                commentService.saveComment(authUser, commentSaveRequestDto)
+        );
+    }
+
+    @PatchMapping("/{commentId}")
+    public CommonResponseDto<?> saveComment(@PathVariable("commentId") Long commentId, @Valid @RequestBody CommentUpdateRequestDto commentUpdateRequestDto){
+        return CommonResponseDto.success(
+                commentService.updateComment(commentId, commentUpdateRequestDto)
+        );
+    }
+
+    @DeleteMapping("/{commentId}")
+    public CommonResponseDto<?> saveComment(@PathVariable("commentId") Long commentId){
+        commentService.deleteComment(commentId);
+        return CommonResponseDto.success(null);
+    }
 }
--- src/main/java/com/sparta/newneoboardbuddy/domain/comment/service/CommentService.java
+++ src/main/java/com/sparta/newneoboardbuddy/domain/comment/service/CommentService.java
@@ -1,9 +1,24 @@
 package com.sparta.newneoboardbuddy.domain.comment.service;
 
 
+import com.sparta.newneoboardbuddy.common.dto.AuthUser;
+import com.sparta.newneoboardbuddy.domain.card.entity.Card;
+import com.sparta.newneoboardbuddy.domain.card.exception.CardNotFoundException;
+import com.sparta.newneoboardbuddy.domain.card.repository.CardRepository;
+import com.sparta.newneoboardbuddy.domain.comment.dto.request.CommentUpdateRequestDto;
+import com.sparta.newneoboardbuddy.domain.comment.dto.request.CommentSaveRequestDto;
+import com.sparta.newneoboardbuddy.domain.comment.dto.response.CommentSaveResponseDto;
+import com.sparta.newneoboardbuddy.domain.comment.dto.response.CommentUpdateResponseDto;
+import com.sparta.newneoboardbuddy.domain.comment.entity.Comment;
+import com.sparta.newneoboardbuddy.domain.comment.exception.CommentNotFoundException;
 import com.sparta.newneoboardbuddy.domain.comment.repository.CommentRepository;
+import com.sparta.newneoboardbuddy.domain.user.entity.User;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import static com.sparta.newneoboardbuddy.domain.user.entity.User.fromAuthUser;
 
 @Service
 @RequiredArgsConstructor
@@ -11,4 +26,85 @@ public class CommentService {
 
     private final CommentRepository commentRepository;
 
+    private final CardRepository cardRepository;
+
+    @Transactional
+    public CommentSaveResponseDto saveComment(AuthUser authUser, CommentSaveRequestDto commentSaveRequestDto) {
+
+        // 카드 유효성 검사
+        Card card = getCardById(commentSaveRequestDto.getCardId());
+
+        // 유저 엔티티 변환
+        User user = fromAuthUser(authUser);
+
+        // 새로운 댓글 저장을 위한 인스턴스 생성
+        Comment comment = Comment.builder()
+                .commentId(commentSaveRequestDto.getCardId())
+                .card(card)
+                .user(user)
+                .comment(commentSaveRequestDto.getComment())
+                .build();
+
+        try {
+            // 댓글 저장
+            Comment newComment = commentRepository.save(comment);
+
+            // 저장된 댓글을 response 로 변환
+            return CommentSaveResponseDto.builder()
+                    .commentId(newComment.getCommentId())
+                    .comment(newComment.getComment())
+                    .createdAt(newComment.getCreatedAt())
+                    .build();
+
+        } catch (RuntimeException e) {
+            throw new RuntimeException("저장도중 알수없는 오류가 발생하였습니다. 다시 시도해주세요.");
+        }
+
+    }
+
+    @Transactional
+    public CommentUpdateResponseDto updateComment(Long commentId, CommentUpdateRequestDto commentUpdateRequestDto) {
+
+        // comment 객체 생성
+        Comment comment = getCommentById(commentId);
+
+        // 업데이트
+        comment.setComment(commentUpdateRequestDto.getComment());
+
+        // 업데이트 된 comment 객체 response 로 변환
+        return CommentUpdateResponseDto.builder()
+                .commentId(comment.getCommentId())
+                .comment(comment.getComment())
+                .updatedAt(comment.getModifiedAt())
+                .build();
+
+    }
+
+    @Transactional
+    public void deleteComment(Long commentId) {
+
+        // comment 객체 생성
+        Comment comment = getCommentById(commentId);
+
+        try {
+            // comment 삭제
+            commentRepository.delete(comment);
+        } catch (RuntimeException e){
+            throw new RuntimeException("댓글 삭제 중 알수없는 오류가 발생하였습니다. 다시 시도해주세요.");
+        }
+    }
+
+    // commentid 로 comment 가 있는지 확인
+    private Comment getCommentById(Long commentId){
+        return commentRepository.findByCommentId(commentId).orElseThrow(
+                () -> new CommentNotFoundException(HttpStatus.BAD_REQUEST)
+        );
+    }
+
+    // cardId 로 card 가 있는지 확인
+    private Card getCardById(Long cardId){
+        return cardRepository.findByCardId(cardId).orElseThrow(
+                () -> new CommentNotFoundException(HttpStatus.BAD_REQUEST)
+        );
+    }
 }
