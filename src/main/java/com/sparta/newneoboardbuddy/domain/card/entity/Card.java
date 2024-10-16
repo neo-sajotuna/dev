3353dcfcc221248e4cc9847a58f68d965944d40b
oht <duduio2050@gmail.com>
2024-10-16 14:53:00 +0900

    [FEAT/FIX] 스프링 실행 파일 Auditing 셋팅, 멤버 테이블에서 유저id, 워크스페이스id 로 데이터 검색하는 쿼리 수정, 파일 서비스 로직 수정, 카드 저장 시 워크스페이스, 보드 엔티티 추가, 카드 등록 시 request 를 requestParam 으로 변경,

--- src/main/java/com/sparta/newneoboardbuddy/domain/card/entity/Card.java
+++ src/main/java/com/sparta/newneoboardbuddy/domain/card/entity/Card.java
@@ -66,7 +66,7 @@ public class Card {
     @JoinColumn(name = "member_id",nullable = false)
     private Member member;
 
-    public Card(String cardTitle, String cardContent, LocalTime startedAt, LocalTime finishedAt, Member member, User user, BoardList list) {
+    public Card(String cardTitle, String cardContent, LocalTime startedAt, LocalTime finishedAt, Member member, User user, BoardList list, Board board, Workspace workspace) {
         this.cardTitle = cardTitle;
         this.cardContent = cardContent;
         this.startedAt = startedAt;
@@ -74,7 +74,7 @@ public class Card {
         this.member = member;
         this.user = user;
         this.boardList = list;
-
-
+        this.board = board;
+        this.workspace = workspace;
     }
 }
